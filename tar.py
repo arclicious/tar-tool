3d3227a72cf046c04813b01fc09c254b755ed8d6
BuildTools <unconfigured@null.spigotmc.org>
2018-11-06 17:53:21 +0000

    Fix code formatting and add to-be-added tar extraction methods

--- tar.py
+++ tar.py
@@ -6,7 +6,7 @@ os.system("clear")
 already_done = 0
 print("[!] To use tar-tool you must have tar and figlet installed.")
 try:
-    os.system("figlet -c -f slant Tar Tool 2.5-snapshotV1")
+    os.system("figlet -c -f slant Tar Tool 2.6-snapshotV1")
 except:
     sys.exit()
 def exit(msg):
@@ -19,25 +19,27 @@ def main():
 		print("1. Extract a tar.gz file")
 		print("2. Extract a tar.bz2 file")
 		print("3. Extract a tar.xz file")
+		print("4. Extract a tgz file")
+		print("5. Extract a bz2 file")
+		print("6. Extract a txz file")
 		print("")
 		inp = raw_input("What would you like to do?")
 		if inp == "1":
 			inp = raw_input("1. What is the name of the file that you would like to extract (without .tar.gz extension)?")
 			print("[*] Extracting file...")			
-			os.system("tar -xzvf %s" % inp + ".tar.gz")
+			os.system("tar -xzvf {}{}".format(inp,".tar.gz"))
 			print("[*] Extracted file!")
 			sys.exit()
 		elif inp == "2":
 			inp = raw_input("1. What is the name of the file that you would like to extract (without .tar.bz2 extension)?")
 			print("[*] Extracting file...")
-			print("tar -jvf %s" % inp + ".tar.bz2")
-			os.system("tar jxf %s" % inp + ".tar.bz2")
+			os.system("tar jxf {}{}".format(inp,".tar.bz2")
 			print("[*] Extracted file!")
 			sys.exit()
 		elif inp == "3":
 			inp = raw_input("1. What is the name of the file that you would like to extract (without .tar.xz extension)?")
 			print("[*] Extracting file...")
-			os.system("tar -xJvf %s" % inp + ".tar.xz")
+			os.system("tar -xJvf {}{}".format(inp,".tar.xz")
 			print("[*] Extracted file!")
 			sys.exit()
 		else:
